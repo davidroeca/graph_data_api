7f7e7d219670aa1143d92d422299f65a941bf33e
davidroeca <david.roeca@gmail.com>
2017-05-22 20:38:32 -0400

    mvp

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
-extern crate serde;
 extern crate serde_json;
 
 #[macro_use] extern crate rocket_contrib;
@@ -24,19 +23,22 @@ struct Node {
 }
 
 #[get("/<id>", format="application/json")]
-fn get_node(id: ID, map: State<GraphMap>) -> Option<JSON<&Node>> {
+fn get_node(id: ID, map: State<GraphMap>) -> JSON<Value> {
     let hash_map = map.lock().expect("Concurrency issue");
-    hash_map.get(&id)
+    match hash_map.get(&id) {
+        Some(node) => JSON(json!({"output": Some(node)})),
+        None => JSON(json!({"output": "No Data"}))
+    }
 }
 
 #[post("/<id>", format = "application/json", data = "<node>")]
-fn post_node(id: ID, node: Node, map: State<GraphMap>) -> Option<JSON<Value>> {
+fn post_node(id: ID, node: JSON<Node>, map: State<GraphMap>) -> JSON<Value> {
     let mut hash_map = map.lock().expect("Concurrency issue");
     if hash_map.contains_key(&id) {
-        None
+        JSON(json!({ "status": "Node already exists"}))
     } else {
-        hash_map.insert(id, node);
-        Some(JSON(json!({ "status": "ok" })))
+        hash_map.insert(id, node.into_inner());
+        JSON(json!({ "status": "ok" }))
     }
 }
 
